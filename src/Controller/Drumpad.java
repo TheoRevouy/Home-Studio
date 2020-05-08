699e8b565cce39c23933873af3ac37655fb8690b
TheoRevouy <revouy@gmail.com>
2020-05-08 13:11:19 +0200

    feat:Link Sample - keyboard touch, Creation loop and different fequency loop

--- src/Controller/Drumpad.java
+++ src/Controller/Drumpad.java
@@ -1,6 +1,8 @@
 package Controller;
 
 import View.View;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -12,16 +14,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.AudioClip;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Drumpad implements Initializable {
+public class Drumpad implements DrumpadKit, Initializable{
 
     @FXML
     public Button backAtHomeButton;
@@ -52,7 +57,7 @@ public class Drumpad implements Initializable {
     public void backAtHome(ActionEvent actionEvent) throws IOException {
         Parent homeViewParent = FXMLLoader.load(getClass().getResource("../Template/home.fxml"));
         Scene homeView = new Scene(homeViewParent);
-        Screen screen=Screen.getPrimary();;
+        Screen screen=Screen.getPrimary();
         Rectangle2D bounds = screen.getVisualBounds();
 
         Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
@@ -70,55 +75,109 @@ public class Drumpad implements Initializable {
 
                 switch (keyEvent.getCode()){
                     case E:
-                        play_audio();
-                        System.out.println("e");
+                        play_audio(DrumpadKit.CLAP);
                         break;
                     case D:
-                        System.out.println("d");
+                        play_audio(DrumpadKit.FX1);
                         break;
                     case C:
-                        System.out.println("c");
+                        play_audio(DrumpadKit.FX2);
                         break;
                     case R:
-                        System.out.println("r");
+                        play_audio(DrumpadKit.HATS);
                         break;
                     case F:
-                        System.out.println("f");
+                        play_audio(DrumpadKit.KICK1);
                         break;
                     case V:
-                        System.out.println("v");
+                        play_audio(DrumpadKit.KICK2);
                         break;
                     case T:
-                        System.out.println("t");
+                        play_audio(DrumpadKit.PERC1);
                         break;
                     case G:
-                        System.out.println("g");
+                        play_audio(DrumpadKit.PERC2);
                         break;
                     case Y:
-                        System.out.println("y");
+                        play_audio(DrumpadKit.PERC3);
                         break;
                     case H:
-                        System.out.println("h");
+                        play_audio(DrumpadKit.PERC4);
                         break;
                     case B:
-                        System.out.println("b");
+                        play_audio(DrumpadKit.SNARE);
                         break;
                     case U:
-                        System.out.println("u");
+                        play_audio(DrumpadKit.STABE);
                         break;
                     case J:
-                        System.out.println("j");
+                        play_audio(DrumpadKit.VOCAL1);
                         break;
                     case N:
-                        System.out.println("n");
+                        play_audio(DrumpadKit.VOCAL2);
                         break;
                   }
             }
         });
     }
 
-    public void play_audio(){
-        AudioClip note = new AudioClip(this.getClass().getResource("../Media/Sound/test.mp3").toString());
+    public void play_audio(String name){
+        AudioClip note = new AudioClip(this.getClass().getResource(name).toString());
         note.play();
+    }
+
+    public void clapLoop(ActionEvent actionEvent) {
+        AudioClip clap = new AudioClip(this.getClass().getResource(DrumpadKit.CLAP).toString());
+        clap.setCycleCount(MediaPlayer.INDEFINITE);
+        clap.play();
+    }
+
+
+    public void loop(Button button, String name) {
+        AudioClip note = new AudioClip(this.getClass().getResource(name).toString());
+
+        button.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if(mouseEvent.getClickCount() % 5 == 1){
+                    System.out.println("1");
+                    changeFrequency(note,5);
+                }else if(mouseEvent.getClickCount() % 5 == 2){
+                    System.out.println("2");
+                    changeFrequency(note,1);
+                }else if(mouseEvent.getClickCount() % 5 == 3){
+                    System.out.println("3");
+                }else if(mouseEvent.getClickCount() % 5 == 4){
+                    System.out.println("4");
+                }else if (mouseEvent.getClickCount() % 5 == 0){
+                    System.out.println("0");
+                }
+            }
+        });
+    }
+
+    public void moussePressed(MouseEvent actionEvent) {
+        loop(drumpadOne, DrumpadKit.CLAP);
+        loop(drumpadTwo, DrumpadKit.FX1);
+        loop(drumpadThree, DrumpadKit.FX2);
+        loop(drumpadFour, DrumpadKit.HATS);
+        loop(drumpadFive, DrumpadKit.KICK1);
+        loop(drumpadSix, DrumpadKit.KICK2);
+        loop(drumpadSeven, DrumpadKit.PERC1);
+        loop(drumpadEight, DrumpadKit.PERC2);
+        loop(drumpadNine, DrumpadKit.PERC3);
+        loop(drumpadTen, DrumpadKit.PERC4);
+        loop(drumpadEleven, DrumpadKit.SNARE);
+        loop(drumpadTwelve, DrumpadKit.STABE);
+        loop(drumpadThirteen, DrumpadKit.VOCAL1);
+        loop(drumpadFourteen, DrumpadKit.VOCAL2);
+    }
+
+    public void changeFrequency(AudioClip note, int duration){
+        Timeline tl = new Timeline(new KeyFrame(Duration.seconds(duration), actionEvent -> note.play()));
+        tl.stop();
+        tl.setCycleCount(Timeline.INDEFINITE);
+        tl.play();
+
     }
 }
---
+++ src/Controller/DrumpadKit.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import javafx.scene.media.AudioClip;
+
+public interface DrumpadKit {
+
+    public final String CLAP = "../Media/Sound/clap.wav";
+
+    public final String FX1 = "../Media/Sound/fx1.wav";
+    public final String FX2 = "../Media/Sound/fx2.wav";
+
+    public final String HATS = "../Media/Sound/hats.wav";
+
+    public final String KICK1 = "../Media/Sound/kick1.wav";
+    public final String KICK2 = "../Media/Sound/kick2.wav";
+
+    public final String PERC1 = "../Media/Sound/percs1.wav";
+    public final String PERC2 = "../Media/Sound/percs2.wav";
+    public final String PERC3 = "../Media/Sound/percs3.wav";
+    public final String PERC4 = "../Media/Sound/percs4.wav";
+
+    public final String SNARE = "../Media/Sound/snare.wav";
+
+    public final String STABE = "../Media/Sound/stabe.wav";
+
+    public final String VOCAL1 = "../Media/Sound/vocal1.wav";
+    public final String VOCAL2 = "../Media/Sound/vocal2.wav";
+}
